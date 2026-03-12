e4e6a7c7c52e463b948f6e271eabe3da2eccffbe
tndat <tuandat05110@gmail.com>
2026-03-12 22:41:48 +0700

    fix: add missing dependencies and refine actions for CI runners

--- app/.cxx/Debug/52r702j2/armeabi-v7a/cmake_install.cmake
+++ app/.cxx/Debug/52r702j2/armeabi-v7a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/tndat/mobile-chatting-app/app/src/main/cpp
+# Install script for directory: /home/tndat/Downloads/mobile-chatting-app/app/src/main/cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/tndat/mobile-chatting-app/app/.cxx/Debug/52r702j2/armeabi-v7a/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/tndat/Downloads/mobile-chatting-app/app/.cxx/Debug/52r702j2/armeabi-v7a/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
